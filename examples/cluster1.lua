9af78e9b2f7e69f2d9e5ebf195d17019f175f757
JingFeng <gejinnian224@163.com>
2019-12-22 22:00:23 +0800

    feat:分析新版本的cluster的用法

--- examples/cluster1.lua
+++ examples/cluster1.lua
@@ -11,12 +11,15 @@ skynet.start(function()
 	local sdb = skynet.newservice("simpledb")
 	-- register name "sdb" for simpledb, you can use cluster.query() later.
 	-- See cluster2.lua
+	--将本地服务sdb暴露到公共接口中,这样其他结点能通过监听的端口来调用暴露出来的API
 	cluster.register("sdb", sdb)
 
 	print(skynet.call(sdb, "lua", "SET", "a", "foobar"))
 	print(skynet.call(sdb, "lua", "SET", "b", "foobar2"))
 	print(skynet.call(sdb, "lua", "GET", "a"))
 	print(skynet.call(sdb, "lua", "GET", "b"))
+
+	--FYD1 cluster1结点开启了两个监听端口,分别是db和db2,用来监听网络中的其他结点的调用
 	cluster.open "db"
 	cluster.open "db2"
 	-- unique snax service
--- examples/cluster2.lua
+++ examples/cluster2.lua
@@ -2,9 +2,11 @@ local skynet = require "skynet"
 local cluster = require "skynet.cluster"
 
 skynet.start(function()
+	--创建远程结点服务的代理
 	local proxy = cluster.proxy "db@sdb"	-- cluster.proxy("db", "@sdb")
 	local largekey = string.rep("X", 128*1024)
 	local largevalue = string.rep("R", 100 * 1024)
+	--调用远程结点暴露的API
 	skynet.call(proxy, "lua", "SET", largekey, largevalue)
 	local v = skynet.call(proxy, "lua", "GET", largekey)
 	assert(largevalue == v)
@@ -19,10 +21,13 @@ skynet.start(function()
 
 	-- test snax service
 	skynet.timeout(300,function()
+		--实时更新网络中的结点
 		cluster.reload {
 			db = false,	-- db is down
 			db3 = "127.0.0.1:2529"
 		}
+		--当远程结点挂掉的时候调用会报错,并且打印调用堆栈 
+		--cluster node [db] is down
 		print(pcall(cluster.call, "db", "@sdb", "GET", "a"))	-- db is down
 	end)
 	cluster.reload { __nowaiting = false }
